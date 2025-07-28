22f1e8e244100d57234cd851d2b5a970a98d3605
shraddha24-smc <chikkalishraddha@gmail.com>
2025-07-29 00:38:53 +0530

    Update s3-usage-alert.sh

--- s3-usage-alert.sh
+++ s3-usage-alert.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+# ----------------------------------------------------
+# AWS S3 Usage Alert Script (with Email Notification)
+# ----------------------------------------------------
 
-# AWS S3 Usage Alert Script
-
+# Set your S3 bucket name
 BUCKET_NAME="your-bucket-name"
+
+# Set usage limit in MB
 LIMIT_MB=4900
+
+# Email to send alerts to
 EMAIL="your.email@example.com"
 
+# Get total size of S3 bucket in bytes and convert to MB
 S3_USAGE_BYTES=$(aws s3 ls s3://$BUCKET_NAME --recursive --summarize | grep "Total Size" | awk '{print $3}')
 S3_USAGE_MB=$((S3_USAGE_BYTES / 1024 / 1024))
 
@@ -17,6 +24,7 @@ if [ "$S3_USAGE_MB" -ge "$LIMIT_MB" ]; then
     exit 1
 fi
 
+# if you want to sync files
 read -p "Do you want to upload your files to S3? (yes/no): " answer
 
 if [ "$answer" == "yes" ]; then
